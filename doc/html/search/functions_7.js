538ce74f2af2a28d3b2b85f00022c60258da1e3e
Milen Mavrov <mmavrov@mm-sol.com>
2018-08-03 16:24:25 +0300

    Fix the documentation

--- doc/html/search/functions_7.js
+++ doc/html/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['intersect',['intersect',['../structconutils_1_1_rect.html#a647f3dff912466850c60d1dd44f6382f',1,'conutils::Rect']]],
-  ['invalidate',['invalidate',['../classconutils_1_1_surface.html#aa259a1d755118d67d8fbd9a65e6c10c5',1,'conutils::Surface::invalidate()'],['../classconutils_1_1_surface.html#a9c50df738120d0b008d5f03352eb785f',1,'conutils::Surface::invalidate(const Rect &amp;bounds)']]]
+  ['invalidate',['invalidate',['../classconutils_1_1_surface.html#aa259a1d755118d67d8fbd9a65e6c10c5',1,'conutils::Surface::invalidate()'],['../classconutils_1_1_surface.html#a9c50df738120d0b008d5f03352eb785f',1,'conutils::Surface::invalidate(const Rect &amp;bounds)'],['../classconutils_1_1_surface.html#ac9052d79efb28a56668d3a553799cc3a',1,'conutils::Surface::invalidate(const size_t start, const size_t end)']]]
 ];
