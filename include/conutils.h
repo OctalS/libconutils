df39f9fac53eea15b889559d15dfe1703abe3b49
Milen Mavrov <mmavrov@mm-sol.com>
2018-08-03 17:26:18 +0300

    Start using the color and attributes enums from Char

--- include/conutils.h
+++ include/conutils.h
@@ -131,9 +131,9 @@
  * Surface child2(2, 2);
  *
  * // Solid fill the surfaces
- * window_bg.fill(Char('.', 2));        // Window background with green dots
- * child1.fill(Char('a', 3));           // Yellow a's
- * child2.fill(Char('b', 4));           // Blue b's
+ * window_bg.fill(Char('.', Char::green));        // Window background with green dots
+ * child1.fill(Char('a', Char::yellow));           // Yellow a's
+ * child2.fill(Char('b', Char::blue));           // Blue b's
  *
  * // Create the tree
  * window.addLayer(&window_bg, 0);      // Add window background with Z order 0
@@ -259,7 +259,7 @@ struct Char {
         white,
     };
 
-    Char(char val = ' ', uint8_t fg = 7, uint8_t bg = 0, uint8_t attr = none)
+    Char(char val = ' ', uint8_t fg = white, uint8_t bg = black, uint8_t attr = none)
         : val(val), fg(fg), bg(bg), attr(attr) { }
 
     inline bool operator== (const Char& rhs) const { return rhs.val == val && rhs.fg == fg && rhs.bg == bg && rhs.attr == attr; }
